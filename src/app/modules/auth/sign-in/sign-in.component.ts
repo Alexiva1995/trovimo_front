61c9af62210f7b5625f792814a91647172365bda
refm.130995 <refm.130995@gmail.com>
2021-01-18 19:14:30 -0400

    Estilos de los formularios  y ajustes del responsive.

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -1,110 +1,128 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  SocialAuthService,
+} from 'angularx-social-login';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
   form: FormGroup;
-  user:any
-  loggedIn:any;
+  user: any;
+  loggedIn: any;
+  submitted = false;
   constructor(
     private socialAuthService: SocialAuthService,
     private _authService: AuthService,
     private fb: FormBuilder,
     private toastr: ToastrService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      email: new FormControl('',[Validators.required]),
-      password: new FormControl('',[Validators.required]),
-      rememberme: new FormControl(false)
-    })
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      rememberme: new FormControl(false),
+    });
     this.socialAuthService.authState.subscribe((user) => {
       this.user = user;
-      this.loggedIn = (user != null);
+      this.loggedIn = user != null;
     });
   }
 
   loginWithFacebook() {
-    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID)
-    .then(res=>{
-      let data = {
-        email:res.email,
-        password:res.email+'#facebook',
-        role:1,
-        register_type:2
-      }
-      this._authService.alterlogin(data).subscribe(
-        res=>{
-          if (!res['user']) {
-            this.toastr.error('User not found');
-          } else {
-            this.toastr.success('Welcome');
+    this.socialAuthService
+      .signIn(FacebookLoginProvider.PROVIDER_ID)
+      .then((res) => {
+        let data = {
+          email: res.email,
+          password: res.email + '#facebook',
+          role: 1,
+          register_type: 2,
+        };
+        this._authService.alterlogin(data).subscribe(
+          (res) => {
+            if (!res['user']) {
+              this.toastr.error('User not found');
+            } else {
+              this.toastr.success('Welcome');
+            }
+            this.form.enable();
+          },
+          (err) => {
+            this.form.enable();
+            console.log(err);
           }
-          this.form.enable()
-        },
-        err=>{
-          this.form.enable()
-          console.log(err)
-        }
-      )
-
-    })
+        );
+      });
   }
 
   loginWithGoogle() {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
-    .then(res=>{
-      let data = {
-        email:res.email,
-        password:res.email+'#google',
-        role:1,
-        register_type:1
-      }
-      this.form.disable()
-      this._authService.alterlogin(data).subscribe(
-        res=>{
-          if (!res['user']) {
-            this.toastr.error('User not found');
-          }else {
-            this.toastr.success('Welcome');
+    this.socialAuthService
+      .signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then((res) => {
+        let data = {
+          email: res.email,
+          password: res.email + '#google',
+          role: 1,
+          register_type: 1,
+        };
+        this.form.disable();
+        this._authService.alterlogin(data).subscribe(
+          (res) => {
+            if (!res['user']) {
+              this.toastr.error('User not found');
+            } else {
+              this.toastr.success('Welcome');
+            }
+            this.form.enable();
+            console.log(res);
+          },
+          (err) => {
+            this.form.enable();
+            console.log(err);
           }
-          this.form.enable()
-          console.log(res)
-        },
-        err=>{
-          this.form.enable()
-          console.log(err)
-        }
-      )
-    })
-  
+        );
+      });
   }
   onSubmit() {
-    this.form.disable()
-    console.log(this.form)
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    this.form.disable();
+    console.log(this.form);
     let data = this.form.value;
-    this._authService.signin(data).subscribe(res=>{
+    this._authService.signin(data).subscribe(
+      (res) => {
         this.form.enable();
-        this.form.reset({})
+        this.form.reset({});
         this.toastr.success('Welcome');
-        localStorage.setItem('access_token',res['access_token'])
-    },err=>{
-      this.form.enable()
-      
-      if (err.status === 401) {
-        
+        localStorage.setItem('access_token', res['access_token']);
+      },
+      (err) => {
+        this.form.enable();
+
+        if (err.status === 401) {
           this.toastr.error('Email or password invalid');
-          
-        
+        }
       }
-    })
+    );
+  }
+
+  get getForm() {
+    return this.form.controls;
   }
 }
--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,33 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService } from 'angularx-social-login';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  FacebookLoginProvider,
+  GoogleLoginProvider,
+  SocialAuthService,
+} from 'angularx-social-login';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
   form: FormGroup;
+  submitted: boolean = false;
   constructor(
     private socialAuthService: SocialAuthService,
     private _authService: AuthService,
-    private fb:FormBuilder,
+    private fb: FormBuilder,
     private toastr: ToastrService
-  ) {
-    
-  }
+  ) {}
 
   ngOnInit(): void {
-    this.form = this.fb.group({
-      email: ['',Validators.required],
-      password: ['',Validators.required],
-      password_confirm: ['',Validators.required],
-      politics: ['',Validators.required]
-    },
-    { validator: this.checkPasswords })
+    this.form = this.fb.group(
+      {
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+        password_confirm: ['', Validators.required],
+        politics: ['', Validators.required],
+      },
+      { validator: this.checkPasswords }
+    );
   }
   checkPasswords(group: FormGroup) {
     // here we have the 'passwords' group
@@ -37,85 +47,90 @@ export class SignUpComponent implements OnInit {
     return pass === confirmPass ? null : { notSame: true };
   }
   onSubmit() {
+    this.submitted = true;
     if (this.form.invalid) {
       return;
     }
     this.form.disable();
     let data = this.form.value;
-    console.log(this.form)
+    console.log(this.form);
     data['register_type'] = 0;
-    data['role'] = 1
-    this._authService.signup(data).subscribe(res=>{
-      console.log(res)
-      this.form.enable()
-      this.form.reset({})
-      this.toastr.success('User created successfully');
-    },err=>{
-      this.form.enable()
-      
-      if (err.status === 401) {
-        
+    data['role'] = 1;
+    this._authService.signup(data).subscribe(
+      (res) => {
+        console.log(res);
+        this.form.enable();
+        this.form.reset({});
+        this.toastr.success('User created successfully');
+      },
+      (err) => {
+        this.form.enable();
+
+        if (err.status === 401) {
           this.toastr.error('Email already taken');
-        
+        }
       }
-    })
+    );
   }
 
   loginWithFacebook() {
-    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID)
-    .then(res=>{
-      let data = {
-        email:res.email,
-        password:res.email+'#facebook',
-        role:1,
-        register_type:2
-      }
-      this._authService.alterlogin(data).subscribe(
-        res=>{
-          if (!res['user']) {
-            this.toastr.error('User not found');
-          } else {
-            this.toastr.success('Welcome');
-            localStorage.setItem('access_token',res['access_token'])
+    this.socialAuthService
+      .signIn(FacebookLoginProvider.PROVIDER_ID)
+      .then((res) => {
+        let data = {
+          email: res.email,
+          password: res.email + '#facebook',
+          role: 1,
+          register_type: 2,
+        };
+        this._authService.alterlogin(data).subscribe(
+          (res) => {
+            if (!res['user']) {
+              this.toastr.error('User not found');
+            } else {
+              this.toastr.success('Welcome');
+              localStorage.setItem('access_token', res['access_token']);
+            }
+            this.form.enable();
+          },
+          (err) => {
+            this.form.enable();
+            console.log(err);
           }
-          this.form.enable()
-        },
-        err=>{
-          this.form.enable()
-          console.log(err)
-        }
-      )
-
-    })
+        );
+      });
   }
 
   loginWithGoogle() {
-    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID)
-    .then(res=>{
-      let data = {
-        email:res.email,
-        password:res.email+'#google',
-        role:1,
-        register_type:1
-      }
-      this.form.disable()
-      this._authService.alterlogin(data).subscribe(
-        res=>{
-          if (!res['user']) {
-            this.toastr.error('User not found');
-          }else {
-            this.toastr.success('Welcome');
-            localStorage.setItem('access_token',res['access_token'])
+    this.socialAuthService
+      .signIn(GoogleLoginProvider.PROVIDER_ID)
+      .then((res) => {
+        let data = {
+          email: res.email,
+          password: res.email + '#google',
+          role: 1,
+          register_type: 1,
+        };
+        this.form.disable();
+        this._authService.alterlogin(data).subscribe(
+          (res) => {
+            if (!res['user']) {
+              this.toastr.error('User not found');
+            } else {
+              this.toastr.success('Welcome');
+              localStorage.setItem('access_token', res['access_token']);
+            }
+            this.form.enable();
+            console.log(res);
+          },
+          (err) => {
+            this.form.enable();
+            console.log(err);
           }
-          this.form.enable()
-          console.log(res)
-        },
-        err=>{
-          this.form.enable()
-          console.log(err)
-        }
-      )
-    })
-  
+        );
+      });
+  }
+  get getForm() {
+    return this.form.controls;
   }
 }
