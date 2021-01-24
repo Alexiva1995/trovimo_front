8933db4520c1fe762b4a0f180bc33bd7608be6e0
hosni <corobonhosni@gmil.com>
2021-01-23 20:10:34 -0400

    correcion de errores y culminacion de la vista find-photos

--- src/app/modules/find-photos/find-photos.component.ts
+++ src/app/modules/find-photos/find-photos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, ElementRef, Renderer2, ViewChild, HostListener
+} from '@angular/core';
+
 
 @Component({
   selector: 'app-find-photos',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FindPhotosComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('grid', { static: true }) grid: ElementRef;
+
+  show: boolean = true;
+  searchValue: string;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    setTimeout(() => this.resizeAllItem(), 1000);
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  getFindPhotosKeyDown(event){
+
+    if(event.keyCode === 13){
+
+      console.log( this.searchValue );
+
+    }
+
+  }
+
+  getFindPhotosClick(){
+      console.log( this.searchValue );
+  }
+
+  @HostListener('window:resize')
+  resizeAllItemonEvent(): void {
+    this.resizeAllItem();
+  }
+
+  resizeAllItem(): void {
+
+    Array.from(this.grid.nativeElement.children).forEach((child: any, i) => {
+      this.resizeGridItem(child);
+    })
+
+  }
+
+  resizeGridItem(child: any): void {
+
+    const rowHeight = this.getParserIntComputedStyle(this.grid.nativeElement, 'grid-auto-rows');
+
+    const rowGap = this.getParserIntComputedStyle(this.grid.nativeElement, 'grid-row-gap');
+
+    const rowSpan = Math.ceil((child.querySelector('.content').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap));
+
+    this.renderer.setStyle(child, 'gridRowEnd', `span ${rowSpan}`);
+
+  }
+
+  getParserIntComputedStyle(element: any, selector: string): number {
+    return parseInt(window.getComputedStyle(element).getPropertyValue(selector));
   }
 
 }
