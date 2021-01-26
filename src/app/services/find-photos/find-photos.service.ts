85d849528c03672c24f69e60c59a454fe9861c0b
hosni <arcaela.reyes@gmail.com>
2021-01-26 17:35:27 -0400

    se itegro la api en find-photos

--- src/app/services/find-photos/find-photos.service.ts
+++ src/app/services/find-photos/find-photos.service.ts
@@ -11,21 +11,9 @@ export class FindPhotosService {
 
   constructor(private http: HttpClient) { }
 
-  getPhotos( searchValues: object ): any {
+  getPhotos( searchValues: any ): any {
 
-    let searchParams = new HttpParams();
-
-    for (const value in searchValues) {
-
-      if (searchValues[value].length !== 0) {
-
-        searchParams.append( value, searchValues[value] );
-
-      }
-
-    }
-
-    return this.http.get( this.api + '/reels/search', { params: searchParams } );
+    return this.http.get( this.api + '/users_experiences?title=' + searchValues.title );
 
   }
 
