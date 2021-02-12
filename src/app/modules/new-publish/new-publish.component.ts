01f3c6440141aa86ceb6bc6b9a378d7fbcc66209
refm.130995 <refm.130995@gmail.com>
2021-02-12 10:32:12 -0400

    sharedSpace id

--- src/app/modules/new-publish/new-publish.component.ts
+++ src/app/modules/new-publish/new-publish.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {MainInfo} from '../../models/main-info';
+import { MainInfo } from '../../models/main-info';
 import Swal from 'sweetalert2';
-import {AuthService} from '../../services/auth/auth.service';
-import {ProjectService} from '../../services/project/project.service';
-import {DetailInfo} from '../../models/detail-info';
+import { AuthService } from '../../services/auth/auth.service';
+import { ProjectService } from '../../services/project/project.service';
+import { DetailInfo } from '../../models/detail-info';
 //import { SharedModel } from 'src/app/models/shared.model';
 
 @Component({
   selector: 'app-new-publish',
   templateUrl: './new-publish.component.html',
-  styleUrls: ['./new-publish.component.scss']
+  styleUrls: ['./new-publish.component.scss'],
 })
 export class NewPublishComponent implements OnInit {
   page = 1;
@@ -30,16 +30,14 @@ export class NewPublishComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private projectService: ProjectService
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   setPage(i: number): void {
     this.page = i;
     if (i === 2 && !this.mainInfo) {
       this.mainInfo = new MainInfo(this.type, this.optionType);
-
     }
     if (i === 3 && !this.detail) {
       this.detail = new DetailInfo();
@@ -47,9 +45,12 @@ export class NewPublishComponent implements OnInit {
   }
   setNamePage(): string {
     switch (this.page) {
-      case 1: return 'Publish';
-      case 2: return 'Main info';
-      case 3: return 'Details';
+      case 1:
+        return 'Publish';
+      case 2:
+        return 'Main info';
+      case 3:
+        return 'Details';
       default:
         return 'publish';
     }
@@ -66,8 +67,10 @@ export class NewPublishComponent implements OnInit {
     }
   }
   disableBtn(): boolean {
-    return (this.page === 1 && this.optionType === 0) ||
-      (this.page === 2 && this.images.length <= 0);
+    return (
+      (this.page === 1 && this.optionType === 0) ||
+      (this.page === 2 && this.images.length <= 0)
+    );
   }
   async onSave() {
     if (!this.authService.isLogged()) {
@@ -84,9 +87,9 @@ export class NewPublishComponent implements OnInit {
       didOpen: () => {
         Swal.showLoading();
         Swal.update({
-          text: 'Saving data project'
+          text: 'Saving data project',
         });
-        
+
         switch (this.mainInfo.option_id) {
           case 1:
             this.optionId = 'new-product';
@@ -99,7 +102,7 @@ export class NewPublishComponent implements OnInit {
             this.optionId = 'new-product';
             this.optionVideoType = 'add-video-product';
             this.optionPhotoType = 'add-photo-product';
-            this.optionalType = 'optional-product'
+            this.optionalType = 'optional-product';
             this.photoType = 'product_id';
             break;
           case 3:
@@ -109,7 +112,7 @@ export class NewPublishComponent implements OnInit {
             this.optionalType = 'optional-shared-space';
             console.log(this.mainInfo);
             this.setSharedSpace();
-            console.log('final ', this.mainInfo)
+            console.log('final ', this.mainInfo);
 
             break;
           case 4:
@@ -121,62 +124,83 @@ export class NewPublishComponent implements OnInit {
           default:
             break;
         }
-        
-        this.projectService.createProject(this.mainInfo, this.optionId)
-          .subscribe( async (response) => {
-            console.log(response); 
-            const id = response.message.id;
-            Swal.update({
-              text: 'Uploading multimedia files'
-            });
-            for await (const video of this.videos) {
-              const formData = new FormData();
-              formData.append('video', video);
-              formData.append(this.photoType, id);
-              this.projectService.uploadVideo(formData, this.optionVideoType).subscribe(responseVideo => {
-                console.log(responseVideo);
-              }, err => {
-                Swal.fire({
-                  title: 'An error ocurred',
-                  icon: 'error'
-                });
+
+        this.projectService
+          .createProject(this.mainInfo, this.optionId)
+          .subscribe(
+            async (response) => {
+              console.log(response);
+              const id = response.message.id;
+              Swal.update({
+                text: 'Uploading multimedia files',
+              });
+              for await (const video of this.videos) {
+                const formData = new FormData();
+                formData.append('video', video);
+                if (this.mainInfo.option_id == 3) {
+                  formData.append('shared_space_id', id);
+                }
+                formData.append(this.photoType, id);
+                this.projectService
+                  .uploadVideo(formData, this.optionVideoType)
+                  .subscribe(
+                    (responseVideo) => {
+                      console.log(responseVideo);
+                    },
+                    (err) => {
+                      Swal.fire({
+                        title: 'An error ocurred',
+                        icon: 'error',
+                      });
+                    }
+                  );
+              }
+              for await (const image of this.images) {
+                const formData = new FormData();
+                formData.append('photo', image);
+                formData.append(this.photoType, id);
+                if (this.mainInfo.option_id == 3) {
+                  formData.append('shared_space_id', id);
+                }
+                this.projectService
+                  .uploadImage(formData, this.optionPhotoType)
+                  .subscribe(
+                    (responseImage) => {
+                      console.log(responseImage);
+                    },
+                    (err) => {
+                      Swal.fire({
+                        title: 'An error ocurred',
+                        icon: 'error',
+                      });
+                    }
+                  );
+              }
+              Swal.update({
+                text: 'Saving detail information',
+              });
+              this.detail.product_id = id;
+              this.projectService
+                .saveDetailInfo(this.detail, this.optionalType)
+                .subscribe((responseDetail) => {});
+              Swal.fire({
+                title: 'Your project has been created successfully',
+                icon: 'success',
+              });
+            },
+            (err) => {
+              console.log(err);
+              Swal.fire({
+                title: 'An error ocurred',
+                icon: 'error',
               });
             }
-            for await (const image of this.images) {
-              const formData = new FormData();
-              formData.append('photo', image);
-              formData.append(this.photoType, id);
-              this.projectService.uploadImage(formData, this.optionPhotoType).subscribe(responseImage => {
-                console.log(responseImage);
-              }, err => {
-                Swal.fire({
-                  title: 'An error ocurred',
-                  icon: 'error'
-                });
-              });
-            }
-            Swal.update({
-              text: 'Saving detail information'
-            });
-            this.detail.product_id = id;
-            this.projectService.saveDetailInfo(this.detail, this.optionalType).subscribe(responseDetail=> {
-            });
-            Swal.fire({
-              title: 'Your project has been created successfully',
-              icon: 'success'
-            });
-        }, err => {
-          console.log(err);
-            Swal.fire({
-              title: 'An error ocurred',
-              icon: 'error'
-            });
-          });
-      }
+          );
+      },
     });
   }
 
-  setSharedSpace(): void{
+  setSharedSpace(): void {
     delete this.mainInfo.peths;
     delete this.mainInfo.furnished;
     delete this.mainInfo.typesp;
